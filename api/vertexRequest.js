34993dd9b12d57449bb8500f518ad76b02e91d43
Kris Bradbury <radthemeswp@gmail.com>
2022-06-21 11:57:12 +0000

    create module for vertex request

---
+++ api/vertexRequest.js
@@ -0,0 +1,34 @@
+/**
+ * trident-hubspot-vertex-middleware - vertexRequest.js
+ * @author Kris Bradbury
+ */
+
+// Import AXIOS
+const axios = require('axios');
+
+// Set Vars
+const endpointBase = 'http://10.1.1.160/TridentServiceTest/api/hubspotapi/';
+
+/**
+ * Sends the request to Vertex
+ * Returns a success / unsuccessful response
+ * @param {strong} endpoint - the endpoint to send the data too
+ * @param {object} data - the data to send
+ */
+async function vertexRequest(endpoint, data) {
+  // console.log(endpoint);
+  // console.table(data);
+
+  let requestConfig = {
+    method: 'post',
+    url: `${endpointBase}${endpoint}`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
+
+  return await axios(requestConfig);
+}
+
+module.exports = vertexRequest;
